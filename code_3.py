6a900d21d07abea7cbc4e41475009519183d9109
LikaShu <142459648+LikaShu@users.noreply.github.com>
2023-12-05 23:16:32 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,40 @@
+import random
+    data = generate_random_data()
+def generate_random_data():
+def main():
+        print(f"Random Number: {item}")
+    return data
+
+
+    main()
+
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":def main():
+
+    return data
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
+import random
+def generate_random_data():    main()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    for item in data:
+
+import random
+def generate_random_data():
+
+def main():
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    return data
